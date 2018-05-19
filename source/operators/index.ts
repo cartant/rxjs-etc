e0e6ed018a13ac033f87c9bee43ad630d0d3e3b9
Nicholas Jamieson <nicholas@cartant.com>
2018-05-19 12:31:03 +1000

    feat(inexorably): Add a better finalize.

--- source/operators/index.ts
+++ source/operators/index.ts
@@ -8,6 +8,7 @@ export * from "./debounceTimeSubsequent";
 export * from "./defaultObservableIfEmpty";
 export * from "./endWith";
 export * from "./guard";
+export * from "./inexorably";
 export * from "./initial";
 export * from "./instanceOf";
 export * from "./pluck";
