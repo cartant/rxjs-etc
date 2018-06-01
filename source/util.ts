1d50d2e3a1d4867665fd3efb5e656725db2aea79
Nicholas Jamieson <nicholas@cartant.com>
2018-06-01 16:50:57 +1000

    feat(isNullish): Add is(Not)Nullish.

--- source/util.ts
+++ source/util.ts
@@ -5,6 +5,14 @@
 
 import { Observable, SchedulerLike } from "rxjs";
 
+export function isNotNullish<T>(value: T | null | undefined): value is T {
+    return (value !== null) && (value !== undefined);
+}
+
+export function isNullish<T>(value: T | null | undefined): value is null | undefined {
+    return (value === null) || (value === undefined);
+}
+
 export function isObservable(value: any): value is Observable<any> {
      return value && (typeof value["subscribe"] === "function");
 }
---
+++ source/utils-spec.ts
@@ -0,0 +1,35 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/rxjs-etc
+ */
+/*tslint:disable:no-unused-expression*/
+
+import { expect } from "chai";
+import { isNotNullish, isNullish } from "./util";
+
+describe("util", () => {
+
+    describe("isNotNullish", () => {
+
+        it("should determine whether a value is not nullish", () => {
+
+            expect(isNotNullish(null)).to.be.false;
+            expect(isNotNullish(undefined)).to.be.false;
+            expect(isNotNullish(false)).to.be.true;
+            expect(isNotNullish(0)).to.be.true;
+            expect(isNotNullish("")).to.be.true;
+        });
+    });
+
+    describe("isNullish", () => {
+
+        it("should determine whether a value is nullish", () => {
+
+            expect(isNullish(null)).to.be.true;
+            expect(isNullish(undefined)).to.be.true;
+            expect(isNullish(false)).to.be.false;
+            expect(isNullish(0)).to.be.false;
+            expect(isNullish("")).to.be.false;
+        });
+    });
+});
