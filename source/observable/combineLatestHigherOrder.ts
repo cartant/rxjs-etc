ba4b0269d8515c87e8d0e6189d978cb8b24cb336
Nicholas Jamieson <nicholas@cartant.com>
2018-08-18 12:07:34 +1000

    feat(combineLatestHigherOrder): Add factory.

---
+++ source/observable/combineLatestHigherOrder.ts
@@ -0,0 +1,71 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/rxjs-etc
+ */
+
+import { Observable, OperatorFunction, Subscription } from "rxjs";
+
+interface Source<T> {
+    completed: boolean;
+    nexted: boolean;
+    observable: Observable<T>;
+    subscription?: Subscription;
+    value?: T;
+}
+
+export function combineLatestHigherOrder<T>(): OperatorFunction<Observable<T>[], T[]> {
+    return higherOrderSource => new Observable<T[]>(observer => {
+        let lasts: Source<T>[] = [];
+        let higherOrderComplete = false;
+        const higherOrderSubscription = new Subscription();
+        higherOrderSubscription.add(higherOrderSource.subscribe(
+            observables => {
+                const subscribes: (() => void)[] = [];
+                const nexts = observables.map(observable => {
+                    const index = lasts.findIndex(l => l.observable === observable);
+                    if (index !== -1) {
+                        const next = lasts[index];
+                        lasts.splice(index, 1);
+                        return next;
+                    }
+                    const next: Source<T> = { completed: false, nexted: false, observable };
+                    subscribes.push(() => {
+                        next.subscription = next.observable.subscribe(
+                            value => {
+                                next.nexted = true;
+                                next.value = value;
+                                if (nexts.every(({ nexted }) => nexted)) {
+                                    observer.next(nexts.map(({ value }) => value) as T[]);
+                                }
+                            },
+                            error => observer.error(error),
+                            () => {
+                                next.completed = true;
+                                if (higherOrderComplete && nexts.every(({ completed }) => completed)) {
+                                    observer.complete();
+                                }
+                            }
+                        );
+                        higherOrderSubscription.add(next.subscription);
+                    });
+                    return next;
+                });
+                lasts.forEach(({ subscription }) => {
+                    if (subscription) {
+                        subscription.unsubscribe();
+                    }
+                });
+                lasts = nexts;
+                subscribes.forEach(subscribe => subscribe());
+            },
+            error => observer.error(error),
+            () => {
+                if (lasts.every(({ completed }) => completed)) {
+                    observer.complete();
+                }
+                higherOrderComplete = true;
+            }
+        ));
+        return higherOrderSubscription;
+    });
+}
