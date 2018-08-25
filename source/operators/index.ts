c5d09d8b14a72bc4cfaca81022cd4905eab564b6
Nicholas Jamieson <nicholas@cartant.com>
2018-08-25 15:41:43 +1000

    feat(bufferRecent): Add operator.

--- source/operators/index.ts
+++ source/operators/index.ts
@@ -3,6 +3,7 @@
  * can be found in the LICENSE file at https://github.com/cartant/rxjs-etc
  */
 
+export * from "./bufferRecent";
 export * from "./debounceAfter";
 export * from "./debounceTimeSubsequent";
 export * from "./defaultObservableIfEmpty";
