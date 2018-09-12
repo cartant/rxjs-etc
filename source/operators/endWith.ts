8a53fe61b2a48c7522690ec19fe5ed477b291da9
Nicholas Jamieson <nicholas@cartant.com>
2018-09-12 18:01:28 +1000

    chore(endWith): Rename type parameters.

--- source/operators/endWith.ts
+++ source/operators/endWith.ts
@@ -6,14 +6,14 @@
 import { concat, from, OperatorFunction, SchedulerLike } from "rxjs";
 import { isScheduler } from "../util";
 
-export function endWith<T, S1 = T>(v1: S1, scheduler?: SchedulerLike): OperatorFunction<T, T | S1>;
-export function endWith<T, S1 = T, S2 = T>(v1: S1, v2: S2, scheduler?: SchedulerLike): OperatorFunction<T, T | S1 | S2>;
-export function endWith<T, S1 = T, S2 = T, S3 = T>(v1: S1, v2: S2, v3: S3, scheduler?: SchedulerLike): OperatorFunction<T, T | S1 | S2 | S3>;
-export function endWith<T, S1 = T, S2 = T, S3 = T, S4 = T>(v1: S1, v2: S2, v3: S3, v4: S4, scheduler?: SchedulerLike): OperatorFunction<T, T | S1 | S2 | S3 | S4>;
-export function endWith<T, S1 = T, S2 = T, S3 = T, S4 = T, S5 = T>(v1: S1, v2: S2, v3: S3, v4: S4, v5: S5, scheduler?: SchedulerLike): OperatorFunction<T, T | S1 | S2 | S3 | S4 | S5>;
-export function endWith<T, S1 = T, S2 = T, S3 = T, S4 = T, S5 = T, S6 = T>(v1: S1, v2: S2, v3: S3, v4: S4, v5: S5, v6: S6, scheduler?: SchedulerLike): OperatorFunction<T, T | S1 | S2 | S3 | S4 | S5 | S6>;
-export function endWith<T, S = T>(...args: (S | SchedulerLike)[]): OperatorFunction<T, T | S>;
-export function endWith<T, S>(...args: (S | SchedulerLike)[]): OperatorFunction<T, T | S> {
+export function endWith<T, E1 = T>(v1: E1, scheduler?: SchedulerLike): OperatorFunction<T, T | E1>;
+export function endWith<T, E1 = T, E2 = T>(v1: E1, v2: E2, scheduler?: SchedulerLike): OperatorFunction<T, T | E1 | E2>;
+export function endWith<T, E1 = T, E2 = T, E3 = T>(v1: E1, v2: E2, v3: E3, scheduler?: SchedulerLike): OperatorFunction<T, T | E1 | E2 | E3>;
+export function endWith<T, E1 = T, E2 = T, E3 = T, E4 = T>(v1: E1, v2: E2, v3: E3, v4: E4, scheduler?: SchedulerLike): OperatorFunction<T, T | E1 | E2 | E3 | E4>;
+export function endWith<T, E1 = T, E2 = T, E3 = T, E4 = T, E5 = T>(v1: E1, v2: E2, v3: E3, v4: E4, v5: E5, scheduler?: SchedulerLike): OperatorFunction<T, T | E1 | E2 | E3 | E4 | E5>;
+export function endWith<T, E1 = T, E2 = T, E3 = T, E4 = T, E5 = T, E6 = T>(v1: E1, v2: E2, v3: E3, v4: E4, v5: E5, v6: E6, scheduler?: SchedulerLike): OperatorFunction<T, T | E1 | E2 | E3 | E4 | E5 | E6>;
+export function endWith<T, E = T>(...args: (E | SchedulerLike)[]): OperatorFunction<T, T | E>;
+export function endWith<T, E>(...args: (E | SchedulerLike)[]): OperatorFunction<T, T | E> {
 
     let scheduler = args[args.length - 1] as (SchedulerLike | null);
     if (isScheduler(scheduler)) {
@@ -21,5 +21,5 @@ export function endWith<T, S>(...args: (S | SchedulerLike)[]): OperatorFunction<
     } else {
         scheduler = null;
     }
-    return source => concat(source, from(args as S[], scheduler as any));
+    return source => concat(source, from(args as E[], scheduler as any));
 }
