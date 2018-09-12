edb36df68d74997e3acb5d2c147f41aa820c0ee7
Nicholas Jamieson <nicholas@cartant.com>
2018-09-12 17:46:17 +1000

    chore(startWith): Loosen typings.

--- source/operators/startWithTimeout.ts
+++ source/operators/startWithTimeout.ts
@@ -3,10 +3,10 @@
  * can be found in the LICENSE file at https://github.com/cartant/rxjs-etc
  */
 
-import { concat, MonoTypeOperatorFunction, race, SchedulerLike, timer } from "rxjs";
+import { concat, OperatorFunction, race, SchedulerLike, timer } from "rxjs";
 import { mapTo, publish } from "rxjs/operators";
 
-export function startWithTimeout<T>(value: T, duration: number | Date, scheduler?: SchedulerLike): MonoTypeOperatorFunction<T> {
+export function startWithTimeout<T, S = T>(value: S, duration: number | Date, scheduler?: SchedulerLike): OperatorFunction<T, S | T> {
     return source => source.pipe(publish(published => race(
         published,
         concat(timer(duration, scheduler).pipe(mapTo(value)), published)
