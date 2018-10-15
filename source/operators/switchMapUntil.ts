9bface787206aa49ee8409546839b42a3e93f039
Nicholas Jamieson <nicholas@cartant.com>
2018-10-16 06:35:10 +1000

    chore(switch): Comment.

--- source/operators/switchMapUntil.ts
+++ source/operators/switchMapUntil.ts
@@ -6,6 +6,11 @@
 import { Observable, OperatorFunction } from "rxjs";
 import { publish, switchMap, takeUntil } from "rxjs/operators";
 
+// This is an operator that applies a time-based operator - like debounceTime -
+// before a switchMap, but unsubscribes the projected observable as soon as the
+// source emits another value. Inspired by:
+// https://blog.strongbrew.io/building-a-safe-autocomplete-operator-with-rxjs/
+
 export function switchMapUntil<T, I, R>(
     prelude: OperatorFunction<T, I>,
     project: (value: I, index: number) => Observable<R>
