96eb375afe2121005e32a0c8056f474b97e79e01
Nicholas Jamieson <nicholas@cartant.com>
2018-11-30 22:04:31 +1000

    chore(package): Upgrade to TS 3.2.

--- source/operators/spread.ts
+++ source/operators/spread.ts
@@ -3,8 +3,8 @@
  * can be found in the LICENSE file at https://github.com/cartant/rxjs-etc
  */
 
-import { OperatorFunction } from "rxjs";
+import { Observable, OperatorFunction } from "rxjs";
 
 export function spread<T, R>(...operations: OperatorFunction<any, any>[]): OperatorFunction<T, R> {
-    return source => operations.reduce((acc, operator) => acc.pipe(operator), source);
+    return (source: Observable<any>) => operations.reduce((acc, operator) => acc.pipe(operator), source);
 }
