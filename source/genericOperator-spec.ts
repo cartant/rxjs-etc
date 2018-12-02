bb5ff679e95b130c8ca6f97dac2f7a012a24aee5
Nicholas Jamieson <nicholas@cartant.com>
2018-12-02 14:00:56 +1000

    chore(generic): Deprecate genericOperator.

--- source/genericOperator-spec.ts
+++ source/genericOperator-spec.ts
@@ -2,7 +2,7 @@
  * @license Use of this source code is governed by an MIT-style license that
  * can be found in the LICENSE file at https://github.com/cartant/rxjs-etc
  */
-/*tslint:disable:no-unused-expression*/
+/*tslint:disable:deprecation no-unused-expression*/
 
 import { pipe } from "rxjs";
 import { delay, distinctUntilChanged } from "rxjs/operators";
