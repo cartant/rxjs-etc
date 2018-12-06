6e492049334a4d28a64362405916001516972365
Nicholas Jamieson <nicholas@cartant.com>
2018-12-06 17:41:41 +1000

    feat(tap): Add flattening tap placeholders.

---
+++ source/operators/switchTap.ts
@@ -0,0 +1,10 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/rxjs-etc
+ */
+
+import { MonoTypeOperatorFunction, ObservableInput } from "rxjs";
+
+export function switchTap<T>(next: (t: T) => ObservableInput<any>, error?: (e: any) => void, complete?: () => void): MonoTypeOperatorFunction<T> {
+    throw new Error("Not implemented");
+}
