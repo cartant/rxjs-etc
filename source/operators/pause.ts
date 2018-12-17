eddc43416e45bb5782de704f56ef5508090bad7e
Nicholas Jamieson <nicholas@cartant.com>
2018-12-17 13:42:05 +1000

    chore(pause): Add comment that mentions Alex.

--- source/operators/pause.ts
+++ source/operators/pause.ts
@@ -6,6 +6,9 @@
 import { MonoTypeOperatorFunction, Observable } from "rxjs";
 import { filter, first, map, mergeMap, publishReplay, startWith } from "rxjs/operators";
 
+// I found this pause operator hiding within a more complicated operator that
+// Alex Okrushko had built and that we were discussing. Thanks, Alex.
+
 export type PausedState = "paused" | "resumed";
 
 export function pause<T>(notifier: Observable<PausedState>, initialState: PausedState = "resumed"): MonoTypeOperatorFunction<T> {
