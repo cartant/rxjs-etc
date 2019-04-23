75ed036ef935a1510a448b3995e69b9876344a2c
Nicholas Jamieson <nicholas@cartant.com>
2019-04-23 17:06:21 +1000

    chore: Add deprecated to all signatures.

--- source/genericPipe.ts
+++ source/genericPipe.ts
@@ -15,27 +15,39 @@ import {
 export function genericPipe<T>(
   ...operators: MonoTypeOperatorFunction<T>[]
 ): <R extends T>(source: Observable<R>) => Observable<R>;
+
+/** @deprecated Inferring a generic can be unsafe; use genericOperator instead */
 export function genericPipe<T>(
   ...operators: UnaryFunction<T, T>[]
 ): <R extends T>(source: R) => R;
+
+/** @deprecated Inferring a generic can be unsafe; use genericOperator instead */
 export function genericPipe<T, A>(
   op1: UnaryFunction<T, A>
 ): UnaryFunction<T, A>;
+
+/** @deprecated Inferring a generic can be unsafe; use genericOperator instead */
 export function genericPipe<T, A, B>(
   op1: UnaryFunction<T, A>,
   op2: UnaryFunction<A, B>
 ): UnaryFunction<T, B>;
+
+/** @deprecated Inferring a generic can be unsafe; use genericOperator instead */
 export function genericPipe<T, A, B, C>(
   op1: UnaryFunction<T, A>,
   op2: UnaryFunction<A, B>,
   op3: UnaryFunction<B, C>
 ): UnaryFunction<T, C>;
+
+/** @deprecated Inferring a generic can be unsafe; use genericOperator instead */
 export function genericPipe<T, A, B, C, D>(
   op1: UnaryFunction<T, A>,
   op2: UnaryFunction<A, B>,
   op3: UnaryFunction<B, C>,
   op4: UnaryFunction<C, D>
 ): UnaryFunction<T, D>;
+
+/** @deprecated Inferring a generic can be unsafe; use genericOperator instead */
 export function genericPipe<T, A, B, C, D, E>(
   op1: UnaryFunction<T, A>,
   op2: UnaryFunction<A, B>,
@@ -43,6 +55,8 @@ export function genericPipe<T, A, B, C, D, E>(
   op4: UnaryFunction<C, D>,
   op5: UnaryFunction<D, E>
 ): UnaryFunction<T, E>;
+
+/** @deprecated Inferring a generic can be unsafe; use genericOperator instead */
 export function genericPipe<T, A, B, C, D, E, F>(
   op1: UnaryFunction<T, A>,
   op2: UnaryFunction<A, B>,
@@ -51,6 +65,8 @@ export function genericPipe<T, A, B, C, D, E, F>(
   op5: UnaryFunction<D, E>,
   op6: UnaryFunction<E, F>
 ): UnaryFunction<T, F>;
+
+/** @deprecated Inferring a generic can be unsafe; use genericOperator instead */
 export function genericPipe<T, A, B, C, D, E, F, G>(
   op1: UnaryFunction<T, A>,
   op2: UnaryFunction<A, B>,
@@ -60,6 +76,8 @@ export function genericPipe<T, A, B, C, D, E, F, G>(
   op6: UnaryFunction<E, F>,
   op7: UnaryFunction<F, G>
 ): UnaryFunction<T, G>;
+
+/** @deprecated Inferring a generic can be unsafe; use genericOperator instead */
 export function genericPipe<T, A, B, C, D, E, F, G, H>(
   op1: UnaryFunction<T, A>,
   op2: UnaryFunction<A, B>,
@@ -70,6 +88,8 @@ export function genericPipe<T, A, B, C, D, E, F, G, H>(
   op7: UnaryFunction<F, G>,
   op8: UnaryFunction<G, H>
 ): UnaryFunction<T, H>;
+
+/** @deprecated Inferring a generic can be unsafe; use genericOperator instead */
 export function genericPipe<T, A, B, C, D, E, F, G, H, I>(
   op1: UnaryFunction<T, A>,
   op2: UnaryFunction<A, B>,
@@ -81,6 +101,8 @@ export function genericPipe<T, A, B, C, D, E, F, G, H, I>(
   op8: UnaryFunction<G, H>,
   op9: UnaryFunction<H, I>
 ): UnaryFunction<T, I>;
+
+/** @deprecated Inferring a generic can be unsafe; use genericOperator instead */
 export function genericPipe<T, A, B, C, D, E, F, G, H, I>(
   op1: UnaryFunction<T, A>,
   op2: UnaryFunction<A, B>,
@@ -93,6 +115,7 @@ export function genericPipe<T, A, B, C, D, E, F, G, H, I>(
   op9: UnaryFunction<H, I>,
   ...operators: UnaryFunction<any, any>[]
 ): UnaryFunction<T, {}>;
+
 export function genericPipe(
   ...operators: UnaryFunction<any, any>[]
 ): UnaryFunction<any, any> {
