4d3c4ae1f5c482a5648e9f48490ab977282d304a
Nicholas Jamieson <nicholas@cartant.com>
2019-06-03 22:21:50 +1000

    test(progress): Use marble tests.

--- source/observable/progress-spec.ts
+++ source/observable/progress-spec.ts
@@ -2,102 +2,98 @@
  * @license Use of this source code is governed by an MIT-style license that
  * can be found in the LICENSE file at https://github.com/cartant/rxjs-etc
  */
-/*tslint:disable:no-console no-unnecessary-callback-wrapper no-unused-expression rxjs-no-ignored-subscription*/
+/*tslint:disable:no-unnecessary-callback-wrapper no-unused-expression*/
 
-import { concat, forkJoin, merge, of, Subject } from "rxjs";
-import { ignoreElements, tap } from "rxjs/operators";
+import { concat, forkJoin, merge } from "rxjs";
+import { marbles } from "rxjs-marbles";
 import { progress } from "./progress";
 
 // prettier-ignore
-describe.only("progress", () => {
-  it("should default to forkJoin", () => {
-    const b = new Subject<string>();
-    progress(
-      [of("a"), b, of("c")],
-      (state, concatenated) => merge(
-        state.pipe(
-          tap({
-            complete: () => console.log("state complete"),
-            next: state => console.log(state)
-          }),
-          ignoreElements()
-        ),
-        concatenated
-      )
-    ).subscribe({
-      complete: () => console.log("complete"),
-      next: value => console.log(value)
+describe("progress", () => {
+  it("should default to forkJoin", marbles(m => {
+    const a = m.cold("        a|         ");
+    const b = m.cold("        --b|       ");
+    const c = m.cold("        ----c|     ");
+    const expected = m.cold(" ijklm(rn|) ", {
+      i: { finalized: 0, nexted: 1, total: 3 },
+      j: { finalized: 1, nexted: 1, total: 3 },
+      k: { finalized: 1, nexted: 2, total: 3 },
+      l: { finalized: 2, nexted: 2, total: 3 },
+      m: { finalized: 2, nexted: 3, total: 3 },
+      n: { finalized: 3, nexted: 3, total: 3 },
+      r: ["a", "b", "c"]
     });
-    b.next("b");
-    b.complete();
-  });
+    const result = progress(
+      [a, b, c],
+      (state, joined) => merge(state, joined)
+    );
+    m.expect(result).toBeObservable(expected);
+  }));
 
-  it("should support forkJoin", () => {
-    const b = new Subject<string>();
-    progress(
-      [of("a"), b, of("c")],
-      (state, concatenated) => merge(
-        state.pipe(
-          tap({
-            complete: () => console.log("state complete"),
-            next: state => console.log(state)
-          }),
-          ignoreElements()
-        ),
-        concatenated
-      ),
+  it("should support forkJoin", marbles(m => {
+    const a = m.cold("        a|         ");
+    const b = m.cold("        --b|       ");
+    const c = m.cold("        ----c|     ");
+    const expected = m.cold(" ijklm(rn|) ", {
+      i: { finalized: 0, nexted: 1, total: 3 },
+      j: { finalized: 1, nexted: 1, total: 3 },
+      k: { finalized: 1, nexted: 2, total: 3 },
+      l: { finalized: 2, nexted: 2, total: 3 },
+      m: { finalized: 2, nexted: 3, total: 3 },
+      n: { finalized: 3, nexted: 3, total: 3 },
+      r: ["a", "b", "c"]
+    });
+    const result = progress(
+      [a, b, c],
+      (state, joined) => merge(state, joined),
       o => forkJoin(...o)
-    ).subscribe({
-      complete: () => console.log("complete"),
-      next: value => console.log(value)
-    });
-    b.next("b");
-    b.complete();
-  });
+    );
+    m.expect(result).toBeObservable(expected);
+  }));
 
-  it("should support concat", () => {
-    const b = new Subject<string>();
-    progress(
-      [of("a"), b, of("c")],
-      (state, concatenated) => merge(
-        state.pipe(
-          tap({
-            complete: () => console.log("state complete"),
-            next: state => console.log(state)
-          }),
-          ignoreElements()
-        ),
-        concatenated
-      ),
+  it("should support concat", marbles(m => {
+    const a = m.cold("        a---|                ");
+    const b = m.cold("            --b---|          ");
+    const c = m.cold("                  --c---|    ");
+    const expected = m.cold(" (ia)j-(kb)l-(mc)(n|) ", {
+      a: "a",
+      b: "b",
+      c: "c",
+      i: { finalized: 0, nexted: 1, total: 3 },
+      j: { finalized: 1, nexted: 1, total: 3 },
+      k: { finalized: 1, nexted: 2, total: 3 },
+      l: { finalized: 2, nexted: 2, total: 3 },
+      m: { finalized: 2, nexted: 3, total: 3 },
+      n: { finalized: 3, nexted: 3, total: 3 }
+    });
+    const result = progress(
+      [a, b, c],
+      (state, joined) => merge(state, joined),
       o => concat(...o)
-    ).subscribe({
-      complete: () => console.log("complete"),
-      next: value => console.log(value)
-    });
-    b.next("b");
-    b.complete();
-  });
+    );
+    m.expect(result).toBeObservable(expected);
+  }));
 
-  it("should support merge", () => {
-    const b = new Subject<string>();
-    progress(
-      [of("a"), b, of("c")],
-      (state, concatenated) => merge(
-        state.pipe(
-          tap({
-            complete: () => console.log("state complete"),
-            next: state => console.log(state)
-          }),
-          ignoreElements()
-        ),
-        concatenated
-      ),
-      o => merge(...o)
-    ).subscribe({
-      complete: () => console.log("complete"),
-      next: value => console.log(value)
+  it("should support merge", marbles(m => {
+    const a = m.cold("        a---|                ");
+    const b = m.cold("        ------b---|          ");
+    const c = m.cold("        ------------c---|    ");
+    const expected = m.cold(" (ia)j-(kb)l-(mc)(n|) ", {
+      a: "a",
+      b: "b",
+      c: "c",
+      i: { finalized: 0, nexted: 1, total: 3 },
+      j: { finalized: 1, nexted: 1, total: 3 },
+      k: { finalized: 1, nexted: 2, total: 3 },
+      l: { finalized: 2, nexted: 2, total: 3 },
+      m: { finalized: 2, nexted: 3, total: 3 },
+      n: { finalized: 3, nexted: 3, total: 3 }
     });
-    b.next("b");
-    b.complete();
-  });
+    const result = progress(
+      [a, b, c],
+      (state, joined) => merge(state, joined),
+      o => merge(...o)
+    );
+    m.expect(result).toBeObservable(expected);
+  }));
 });
