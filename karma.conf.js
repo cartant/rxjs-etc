ededc200c89898fb034d11a6270e2cd4b9054754
Nicholas Jamieson <nicholas@cartant.com>
2019-10-06 10:25:52 +1000

    chore(ci): Use image with browsers.

--- karma.conf.js
+++ karma.conf.js
@@ -2,38 +2,35 @@
 
 process.env.CHROME_BIN = require("puppeteer").executablePath();
 
-exports = module.exports = function (config) {
-
-    config.set({
-        basePath: "",
-        browsers: ["ChromeHeadlessNoSandbox"],
-        colors: true,
-        concurrency: Infinity,
-        customLaunchers: {
-            ChromeHeadlessNoSandbox: {
-                base: "ChromeHeadless",
-                flags: ["--no-sandbox"]
-            }
-        },
-        exclude: [],
-        files: [
-            { pattern: "source/**/*-spec.ts", watched: false }
-        ],
-        frameworks: ["mocha"],
-        logLevel: config.LOG_INFO,
-        mime : {
-            "text/x-typescript": ["ts"]
-        },
-        port: 9876,
-        preprocessors: {
-            "source/**/*-spec.ts": ["webpack"]
-        },
-        proxies: {},
-        reporters: ["spec"],
-        webpack: require("./webpack.config.test")({}),
-        webpackMiddleware: {
-            noInfo: true,
-            stats: "errors-only"
-        }
-    });
+exports = module.exports = function(config) {
+  config.set({
+    basePath: "",
+    browsers: ["ChromeHeadlessNoSandbox"],
+    colors: true,
+    concurrency: Infinity,
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox --disable-setuid-sandbox"]
+      }
+    },
+    exclude: [],
+    files: [{ pattern: "source/**/*-spec.ts", watched: false }],
+    frameworks: ["mocha"],
+    logLevel: config.LOG_INFO,
+    mime: {
+      "text/x-typescript": ["ts"]
+    },
+    port: 9876,
+    preprocessors: {
+      "source/**/*-spec.ts": ["webpack"]
+    },
+    proxies: {},
+    reporters: ["spec"],
+    webpack: require("./webpack.config.test")({}),
+    webpackMiddleware: {
+      noInfo: true,
+      stats: "errors-only"
+    }
+  });
 };
