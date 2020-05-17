f8a7e2fea004bd9eb3bb0c64658cb15df02d62f4
Nicholas Jamieson <nicholas@cartant.com>
2020-05-17 14:18:38 +1000

    fix: Incorporate Exclude in splitBy signature.
    As Moshe used it in https://github.com/ReactiveX/rxjs/pull/5441

--- source/operators/splitBy.ts
+++ source/operators/splitBy.ts
@@ -9,7 +9,7 @@ import { bucketBy } from "./bucketBy";
 export function splitBy<T, S extends T>(
   predicate: (value: T, index: number) => value is S,
   subjectSelector?: () => Subject<T>
-): OperatorFunction<T, [Observable<S>, Observable<T>]>;
+): OperatorFunction<T, [Observable<S>, Observable<Exclude<T, S>>]>;
 
 export function splitBy<T>(
   predicate: (value: T, index: number) => boolean,
@@ -20,11 +20,9 @@ export function splitBy<T>(
   predicate: (value: T, index: number) => boolean,
   subjectSelector: () => Subject<T> = () => new Subject<T>()
 ): OperatorFunction<T, [Observable<T>, Observable<T>]> {
-  return (
-    bucketBy(
-      2,
-      (value, index) => (predicate(value, index) ? 0 : 1),
-      subjectSelector
-    ) as OperatorFunction<T, [Observable<T>, Observable<T>]>
-  );
+  return bucketBy(
+    2,
+    (value, index) => (predicate(value, index) ? 0 : 1),
+    subjectSelector
+  ) as OperatorFunction<T, [Observable<T>, Observable<T>]>;
 }
